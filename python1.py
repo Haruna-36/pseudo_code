1c93f2c91f4abd757a0b19cf0c8f9f7eb06aae8e
uchiikeharuna <uchiike.haruna.s7@dc.tohoku.ac.jp>
2025-07-04 14:28:20 +0900

    fix python code

--- python1.py
+++ python1.py
@@ -1,7 +1,9 @@
 from numpy import empty
 n = 100
-a = np.empty(n)
-result = 0
-for i in range(0, n, 1):
-    result = result + a[i]
-print(result)
+a = empty(n)
+for j in range(1, n+1):
+    a[j-1] = j
+result1 = 0
+for i in range(0, n):
+    result1 = result1 + a[i]
+print(result1)
