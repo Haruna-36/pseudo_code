2ce57471383de156824e07a12f13fa863661b1b0
uchiikeharuna <uchiike.haruna.s7@dc.tohoku.ac.jp>
2025-07-04 15:16:18 +0900

    change to function

--- python1.py
+++ python1.py
@@ -1,9 +1,6 @@
 from numpy import empty
-n = 100
-a = empty(n)
-for j in range(1, n+1):
-    a[j-1] = j
-result1 = 0
-for i in range(0, n):
-    result1 = result1 + a[i]
-print(result1)
+def sum_practice(n, a):
+    result_sum = 0
+    for i in range(0, n):
+       result_sum = result_sum + a[i]
+    return result_sum
