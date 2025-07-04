8a4ce1fa0f8c80d3018447e6b4b5f8716fe551a7
uchiikeharuna <uchiike.haruna.s7@dc.tohoku.ac.jp>
2025-07-04 15:16:56 +0900

    fix code

--- python2.py
+++ python2.py
@@ -1,4 +1,10 @@
+import python1
+
 from numpy import empty
-result2 = 0
-result2 = result1/100
-print(result2)
+n = 100
+a = empty(n)
+for j in range(1, n+1):
+    a[j-1] = j
+average = 0
+average = python1.sum_practice(n, a)/100
+print(average)
